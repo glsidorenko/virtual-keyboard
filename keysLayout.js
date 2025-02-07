50b2c6ceef1d4629069af2570b1abe8d8c12a3de
Gleb <glsidorenko01@gmail.com>
2025-02-07 13:53:18 +0300

    rewrite structure using Map

--- keysLayout.js
+++ keysLayout.js
@@ -1,738 +1,211 @@
-// export const keys = {
-//     Backquote: {
-//         en: {
-//             caseDown: '`',
-//             caseUp: '~',
-//         },
-//         ru: {
-//             key: 'ё',
-//         },
-//     },
-//     Digit1: {
-//         en: {
-//             caseDown: '1',
-//             caseUp: '!',
-//         },
-//     },
-//     Digit2: {
-//         en: {
-//             caseDown: '2',
-//             caseUp: '@',
-//         },
-//     },
-//     Digit3: {
-//         en: {
-//             caseDown: '3',
-//             caseUp: '#',
-//
-//         },
-//     },
-//     Digit4: {
-//         en: {
-//             caseDown: '4',
-//             caseUp: '$',
-//         },
-//     },
-//     Digit5: {
-//         en: {
-//             caseDown: '5',
-//             caseUp: '%',
-//         },
-//     },
-//     Digit6: {
-//         en: {
-//             caseDown: '6',
-//             caseUp: '^',
-//
-//         },
-//
-//     },
-//     Digit7: {
-//         en: {
-//             caseDown: '7',
-//             caseUp: '&',
-//         },
-//     },
-//     Digit8: {
-//         en: {
-//             caseDown: '8',
-//             caseUp: '*',
-//         },
-//     },
-//     Digit9: {
-//         en: {
-//             caseDown: '9',
-//             caseUp: '(',
-//         },
-//     },
-//     Digit0: {
-//         en: {
-//             caseDown: '0',
-//             caseUp: ')',
-//         },
-//
-//     },
-//     Minus: {
-//         en: {
-//             caseDown: '-',
-//             caseUp: '_',
-//         },
-//
-//     },
-//     Equal: {
-//         en: {
-//             caseDown: '=',
-//             caseUp: '+',
-//         },
-//
-//     },
-//     Backspace: {
-//         en: {
-//             caseDown: 'Backspace',
-//         },
-//     },
-//
-//     // второй ряд
-//
-//     Tab: {
-//         en: {
-//             caseDown: 'Tab',
-//
-//         },
-//         ru: {
-//             caseDown: 'Tab',
-//         },
-//     },
-//     KeyQ: {
-//         en: {
-//             caseDown: 'q',
-//
-//         },
-//         ru: {
-//             caseDown: 'й',
-//
-//         },
-//     },
-//     KeyW: {
-//         en: {
-//             caseDown: 'w',
-//
-//         },
-//         ru: {
-//             caseDown: 'ц',
-//
-//         },
-//     },
-//     KeyE: {
-//         en: {
-//             caseDown: 'e',
-//
-//         },
-//         ru: {
-//             caseDown: 'у',
-//
-//         },
-//     },
-//     KeyR: {
-//         en: {
-//             caseDown: 'r',
-//
-//         },
-//         ru: {
-//             caseDown: 'к',
-//
-//         },
-//     },
-//     KeyT: {
-//         en: {
-//             caseDown: 't',
-//
-//         },
-//         ru: {
-//             caseDown: 'е',
-//
-//         },
-//     },
-//     KeyY: {
-//         en: {
-//             caseDown: 'y',
-//
-//         },
-//         ru: {
-//             caseDown: 'н',
-//
-//         },
-//     },
-//     KeyU: {
-//         en: {
-//             caseDown: 'u',
-//
-//         },
-//         ru: {
-//             caseDown: 'г',
-//
-//         },
-//     },
-//     KeyI: {
-//         en: {
-//             caseDown: 'i',
-//
-//         },
-//         ru: {
-//             caseDown: 'ш',
-//
-//         },
-//     },
-//     KeyO: {
-//         en: {
-//             caseDown: 'o',
-//
-//         },
-//         ru: {
-//             caseDown: 'щ',
-//
-//         },
-//     },
-//     KeyP: {
-//         en: {
-//             caseDown: 'p',
-//
-//         },
-//         ru: {
-//             caseDown: 'з',
-//
-//         },
-//     },
-//     BracketLeft: {
-//         en: {
-//             caseDown: '[',
-//             caseUp: '{',
-//         },
-//         ru: {
-//             caseDown: 'х',
-//
-//         },
-//     },
-//     BracketRight: {
-//         en: {
-//             caseDown: ']',
-//             caseUp: '}',
-//         },
-//         ru: {
-//             caseDown: 'ъ',
-//
-//         },
-//     },
-//     Backslash: {
-//         en: {
-//             caseDown: '\\',
-//             caseUp: '|',
-//             caps: '\\',
-//             shiftCas: '|',
-//         },
-//         ru: {
-//             caseDown: '\\',
-//             caseUp: '|',
-//             caps: '\\',
-//             shiftCas: '|',
-//         },
-//     },
-//     Delete: {
-//         en: {
-//             caseDown: 'Del',
-//
-//         },
-//         ru: {
-//             caseDown: 'Del',
-//
-//         },
-//     },
-//
-//
-//     // третий ряд
-//
-//     CapsLock: {
-//         en: {
-//             caseDown: 'CapsLock',
-//
-//         },
-//         ru: {
-//             caseDown: 'CapsLock',
-//
-//         },
-//     },
-//     KeyA: {
-//         en: {
-//             caseDown: 'a',
-//
-//         },
-//         ru: {
-//             caseDown: 'ф',
-//
-//         },
-//     },
-//     KeyS: {
-//         en: {
-//             caseDown: 's',
-//
-//         },
-//         ru: {
-//             caseDown: 'ы',
-//
-//         },
-//     },
-//     KeyD: {
-//         en: {
-//             caseDown: 'd',
-//
-//         },
-//         ru: {
-//             caseDown: 'в',
-//
-//         },
-//     },
-//     KeyF: {
-//         en: {
-//             caseDown: 'f',
-//
-//         },
-//         ru: {
-//             caseDown: 'а',
-//         },
-//     },
-//     KeyG: {
-//         en: {
-//             caseDown: 'g',
-//         },
-//         ru: {
-//             caseDown: 'п',
-//         },
-//     },
-//     KeyH: {
-//         en: {
-//             caseDown: 'h',
-//         },
-//         ru: {
-//             caseDown: 'р',
-//         },
-//     },
-//     KeyJ: {
-//         en: {
-//             caseDown: 'j',
-//         },
-//         ru: {
-//             caseDown: 'о',
-//         },
-//     },
-//     KeyK: {
-//         en: {
-//             caseDown: 'k',
-//         },
-//         ru: {
-//             caseDown: 'л',
-//         },
-//     },
-//     KeyL: {
-//         en: {
-//             caseDown: 'l',
-//         },
-//         ru: {
-//             caseDown: 'д',
-//         },
-//     },
-//     Semicolon: {
-//         en: {
-//             caseDown: ';',
-//             caseUp: ':',
-//
-//         },
-//         ru: {
-//             caseDown: 'ж',
-//         },
-//     },
-//     Quote: {
-//         en: {
-//             caseDown: "'",
-//             caseUp: '"',
-//
-//         },
-//         ru: {
-//             caseDown: 'э',
-//
-//         },
-//     },
-//     Enter: {
-//         en: {
-//             caseDown: 'Enter',
-//
-//         },
-//         ru: {
-//             caseDown: 'Enter',
-//
-//         },
-//     },
-//
-//     // четвертый ряд
-//
-//     ShiftLeft: {
-//         en: {
-//             caseDown: 'Shift',
-//
-//         },
-//         ru: {
-//             caseDown: 'Shift',
-//
-//         },
-//     },
-//     KeyZ: {
-//         en: {
-//             caseDown: 'z',
-//
-//         },
-//         ru: {
-//             caseDown: 'я',
-//
-//         },
-//     },
-//     KeyX: {
-//         en: {
-//             caseDown: 'x',
-//
-//         },
-//         ru: {
-//             caseDown: 'ч',
-//
-//         },
-//     },
-//     KeyC: {
-//         en: {
-//             caseDown: 'c',
-//
-//         },
-//         ru: {
-//             caseDown: 'с',
-//
-//         },
-//     },
-//     KeyV: {
-//         en: {
-//             caseDown: 'v',
-//
-//         },
-//         ru: {
-//             caseDown: 'м',
-//
-//         },
-//     },
-//     KeyB: {
-//         en: {
-//             caseDown: 'b',
-//
-//         },
-//         ru: {
-//             caseDown: 'и',
-//
-//         },
-//     },
-//     KeyN: {
-//         en: {
-//             caseDown: 'n',
-//
-//         },
-//         ru: {
-//             caseDown: 'т',
-//
-//         },
-//     },
-//     KeyM: {
-//         en: {
-//             caseDown: 'm',
-//
-//         },
-//         ru: {
-//             caseDown: 'о',
-//
-//         },
-//     },
-//     Comma: {
-//         en: {
-//             caseDown: ',',
-//             caseUp: '<',
-//         },
-//         ru: {
-//             caseDown: 'б',
-//
-//         },
-//     },
-//     Period: {
-//         en: {
-//             caseDown: '.',
-//             caseUp: '>',
-//         },
-//         ru: {
-//             caseDown: 'ю',
-//         },
-//     },
-//     Slash: {
-//         en: {
-//             caseDown: '/',
-//             caseUp: '?',
-//
-//         },
-//         ru: {
-//             caseDown: '.',
-//             caseUp: ',',
-//         },
-//     },
-//     ArrowUp: {
-//         en: {
-//             caseDown: '▲',
-//         },
-//         ru: {
-//             caseDown: '▲',
-//         },
-//     },
-//     ShiftRight: {
-//         en: {
-//             caseDown: 'Shift',
-//
-//         },
-//         ru: {
-//             caseDown: 'Shift',
-//         },
-//     },
-//
-//     // пятый ряд
-//
-//     ControlLeft: {
-//         en: {
-//             caseDown: 'Ctrl',
-//
-//         },
-//         ru: {
-//             caseDown: 'Ctrl',
-//         },
-//     },
-//     MetaLeft: {
-//         en: {
-//             caseDown: 'Win',
-//         },
-//         ru: {
-//             caseDown: 'Win',
-//         },
-//     },
-//     AltLeft: {
-//         en: {
-//             caseDown: 'Alt',
-//
-//         },
-//         ru: {
-//             caseDown: 'Alt',
-//         },
-//     },
-//     Space: {
-//         en: {
-//             caseDown: ' ',
-//
-//         },
-//         ru: {
-//             caseDown: ' ',
-//         },
-//     },
-//     AltRight: {
-//         en: {
-//             caseDown: 'Alt',
-//
-//         },
-//         ru: {
-//             caseDown: 'Alt',
-//
-//         },
-//     },
-//     ArrowLeft: {
-//         en: {
-//             caseDown: '◄',
-//         },
-//         ru: {
-//             caseDown: '◄',
-//         },
-//     },
-//     ArrowDown: {
-//         en: {
-//             caseDown: '▼',
-//         },
-//         ru: {
-//             caseDown: '▼',
-//         },
-//     },
-//     ArrowRight: {
-//         en: {
-//             caseDown: '►',
-//         },
-//         ru: {
-//             caseDown: '►',
-//         },
-//     },
-//     ControlRight: {
-//         en: {
-//             caseDown: 'Ctrl',
-//         },
-//         ru: {
-//             caseDown: 'Ctrl',
-//         },
-//     },
-// };
+const engLayout = {
 
-export const keys = [
     // Первый ряд
-    ['Backquote', '`'],
-    ['Digit1', '1'],
-    ['Digit2', '2'],
-    ['Digit3', '3'],
-    ['Digit4', '4'],
-    ['Digit5', '5'],
-    ['Digit6', '6'],
-    ['Digit7', '7'],
-    ['Digit8', '8'],
-    ['Digit9', '9'],
-    ['Digit0', '0'],
-    ['Minus', '-'],
-    ['Equal', '='],
-    ['Backspace', 'Backspace'],
+
+    Backquote: '`',
+    Digit1: '1',
+    Digit2: '2',
+    Digit3: '3',
+    Digit4: '4',
+    Digit5: '5',
+    Digit6: '6',
+    Digit7: '7',
+    Digit8: '8',
+    Digit9: '9',
+    Digit0: '0',
+    Minus: '-',
+    Equal: '=',
+    Backspace: 'Backspace',
 
     // Второй ряд
-    ["Tab", "Tab"],
-    ["KeyQ", "q"],
-    ["KeyW", "w"],
-    ["KeyE", "e"],
-    ["KeyR", "r"],
-    ["KeyT", "t"],
-    ["KeyY", "y"],
-    ["KeyU", "u"],
-    ["KeyI", "i"],
-    ["KeyO", "o"],
-    ["KeyP", "p"],
-    ['BracketLeft', "["],
-    ['BracketRight', "]"],
-    ['Backslash', "\\"],
-    ['Delete', "Del"],
+    Tab: "Tab",
+    KeyQ: "q",
+    KeyW: "w",
+    KeyE: "e",
+    KeyR: "r",
+    KeyT: "t",
+    KeyY: "y",
+    KeyU: "u",
+    KeyI: "i",
+    KeyO: "o",
+    KeyP: "p",
+    BracketLeft: "[",
+    BracketRight: "]",
+    Backslash: "\\",
+    Delete: "Del",
 
-    // Третий ряд
-    ['CapsLock', "CapsLock"],
-    ["KeyA", "a"],
-    ["KeyS", "s"],
-    ["KeyD", "d"],
-    ["KeyF", "f"],
-    ["KeyG", "g"],
-    ["KeyH", "h"],
-    ["KeyJ", "j"],
-    ["KeyK", "k"],
-    ["KeyL", "l"],
-    ['Semicolon', ";"],
-    ['Quote', "'"],
-    ['Enter', "Enter"],
+    // Третий ря
+
+    CapsLock: "CapsLock",
+    KeyA: "a",
+    KeyS: "s",
+    KeyD: "d",
+    KeyF: "f",
+    KeyG: "g",
+    KeyH: "h",
+    KeyJ: "j",
+    KeyK: "k",
+    KeyL: "l",
+    Semicolon: ";",
+    Quote: "'",
+    Enter: "Enter",
 
     // Четвертый ряд
-    ["ShiftLeft", "Shift"],
-    ["KeyZ", "z"],
-    ["KeyX", "x"],
-    ["KeyC", "c"],
-    ["KeyV", "v"],
-    ["KeyB", "b"],
-    ["KeyN", "n"],
-    ["KeyM", "m"],
-    ["Comma", ","],
-    ['Period', "."],
-    ["Slash", "/"],
-    ['ArrowUp', "▲"],
-    ['ShiftRight', "Shift"],
+
+    ShiftLeft: "Shift",
+    KeyZ: "z",
+    KeyX: "x",
+    KeyC: "c",
+    KeyV: "v",
+    KeyB: "b",
+    KeyN: "n",
+    KeyM: "m",
+    Comma: ",",
+    Period: ".",
+    Slash: "/",
+    ArrowUp: "▲",
+    ShiftRight: "Shift",
 
     // Пятый ряд
-    ["ControlLeft", "Ctrl"],
-    ['MetaLeft', "Win"],
-    ['AltLeft', "Alt"],
-    ['Space', " "],
-    ['AltRight', "Alt"],
-    ['ArrowLeft', "◄"],
-    ['ArrowDown', "▼"],
-    ['ArrowRight', "►"],
-    ['ControlRight', "Ctrl"],
-];
 
-// export const newKeys = [
-//     [192, "`"],
-//     [49, "1"],
-//     [50, "2"],
-//     [51, "3"],
-//     [52, "4"],
-//     [53, "5"],
-//     [54, "6"],
-//     [55, "7"],
-//     [56, "8"],
-//     [57, "9"],
-//     [48, "0"],
-//     [189, "-"],
-//     [187, "="],
-//     [8, "Backspace"],
-//
-//     //выше сделано
-//
-//     //second line
-//     [9, "Tab"],
-//     [81, "q"],
-//     [87, "w"],
-//     [69, "e"],
-//     [82, "r"],
-//     [84, "t"],
-//     [89, "y"],
-//     [85, "u"],
-//     [73, "i"],
-//     [79, "o"],
-//     [80, "p"],
-//     [219, "["],
-//     [221, "]"],
-//     [220, "\\"],
-//     [46, "Del"],
-//
-//     //third line
-//     [20, "CapsLock"],
-//     [65, "a"],
-//     [83, "s"],
-//     [68, "d"],
-//     [70, "f"],
-//     [71, "g"],
-//     [72, "h"],
-//     [74, "j"],
-//     [75, "k"],
-//     [76, "l"],
-//     [186, ";"],
-//     [222, "'"],
-//     [13, "Enter"],
-//
-//     //4-lien
-//     [16, "Shift"],
-//     [90, "z"],
-//     [88, "x"],
-//     [67, "c"],
-//     [86, "v"],
-//     [66, "b"],
-//     [78, "n"],
-//     [77, "m"],
-//     [188, ","],
-//     [190, "."],
-//     [191, "/"],
-//     [38, "▲"],
-//     ['16', "Shift"],
-//
-//     //5 line
-//
-//     [17, "Control"],
-//     [91, "Win"],
-//     [18, "Alt"],
-//     [32, "Space"],
-//     ['18', "Alt"],
-//     [37, "◄"],
-//     [40, "▼"],
-//     [39, "►"],
-//     ['17', "Control"],
-// ]
+    ControlLeft: "Ctrl",
+    MetaLeft: "Win",
+    AltLeft: "Alt",
+    Space: " ",
+    AltRight: "Alt",
+    ArrowLeft: "◄",
+    ArrowDown: "▼",
+    ArrowRight: "►",
+    ControlRight: "Ctrl",
+};
+const rusLayout = {
+
+    // Первый ряд
+
+    Backquote: 'ё',
+    Digit1: '1',
+    Digit2: '2',
+    Digit3: '3',
+    Digit4: '4',
+    Digit5: '5',
+    Digit6: '6',
+    Digit7: '7',
+    Digit8: '8',
+    Digit9: '9',
+    Digit0: '0',
+    Minus: '-',
+    Equal: '=',
+    Backspace: 'Backspace',
+
+    // Второй ряд
+
+    Tab: "Tab",
+    KeyQ: "й",
+    KeyW: "ц",
+    KeyE: "у",
+    KeyR: "к",
+    KeyT: "е",
+    KeyY: "н",
+    KeyU: "г",
+    KeyI: "ш",
+    KeyO: "щ",
+    KeyP: "з",
+    BracketLeft: "х",
+    BracketRight: "ъ",
+    Backslash: "\\",
+    Delete: "Del",
+
+    // Третий ряд
+
+    CapsLock: "CapsLock",
+    KeyA: "ф",
+    KeyS: "ы",
+    KeyD: "в",
+    KeyF: "а",
+    KeyG: "п",
+    KeyH: "р",
+    KeyJ: "о",
+    KeyK: "л",
+    KeyL: "д",
+    Semicolon: "ж",
+    Quote: "э",
+    Enter: "Enter",
+
+    // Четвертый ряд
+
+    ShiftLeft: "Shift",
+    KeyZ: "я",
+    KeyX: "ч",
+    KeyC: "с",
+    KeyV: "м",
+    KeyB: "и",
+    KeyN: "т",
+    KeyM: "ь",
+    Comma: "б",
+    Period: "ю",
+    Slash: ".",
+    ArrowUp: "▲",
+    ShiftRight: "Shift",
+
+    // Пятый ряд
+
+    ControlLeft: "Ctrl",
+    MetaLeft: "Win",
+    AltLeft: "Alt",
+    Space: " ",
+    AltRight: "Alt",
+    ArrowLeft: "◄",
+    ArrowDown: "▼",
+    ArrowRight: "►",
+    ControlRight: "Ctrl",
+};
+
+const engExtraKeys = {
+    Backquote:  '~',
+    Digit1:  '!',
+    Digit2:  '@',
+    Digit3:  '#',
+    Digit4:  '$',
+    Digit5:  '%',
+    Digit6:  '^',
+    Digit7:  '&',
+    Digit8:  '*',
+    Digit9:  '(',
+    Digit0:  ')',
+    Minus:  '_',
+    Equal:  '+',
+    BracketLeft:  "{",
+    BracketRight:  "}",
+    Backslash:  "|",
+    Semicolon:  ":",
+    Quote:  `"`,
+    Comma:  "<",
+    Period:  ">",
+    Slash:  "?",
+};
+const rusExtraKeys = {
+    Digit1:  '!',
+    Digit2:  '"',
+    Digit3:  '№',
+    Digit4:  ';',
+    Digit5:  '%',
+    Digit6:  ':',
+    Digit7:  '?',
+    Digit8:  '*',
+    Digit9:  '(',
+    Digit0:  ')',
+    Minus:  '_',
+    Equal:  '+',
+    Backslash:  "/",
+    Slash:  ",",
+};
+
+export const keysMap = new Map();
+keysMap.set('rus', {
+    keys: rusLayout,
+    extraKeys: rusExtraKeys,
+});
+keysMap.set('eng', {
+    keys: engLayout,
+    extraKeys: engExtraKeys,
+});
